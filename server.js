f34ad4a5580c449f93ce164aba039d73db1c45c5
anumberplate <aluma.kyle@gmail.com>
2025-07-26 05:29:16 +0300

    Middleware

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const app = express()
-
+const errorHandler = require('./middleware/errorHandler')
+const { logger } = require('./middleware/logEvents')
 const PORT = process.env.PORT || 3500
+
+//custom middleware logger
+
+app.use(logger)
+
+const whitelist = ['https://www.yoursite.com','http://127.0.0.1:5500', 'http://localhost:3500']
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin){
+      callback(null, true)
+    } else{
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get(/^\/$|\/index(.html)?$/, (req, res) => {
   //res.sendFile('./views/index.html', { root: __dirname })
@@ -38,12 +64,22 @@ const three = (req, res) => {
 
 app.get(/^\/chain(.html)?$/, [one, two, three])
 
-app.get(/\/*/, (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.all(/\/*/, (req, res) => {
+  res.status(404)
+  if (req.accepts('html')){
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')){
+    res.send({error: "404 not found"})
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
+  
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
+
+app.use(errorHandler)
 // add listener for the log event
 
 
