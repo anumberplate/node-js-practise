327fa5d0931c6a2c5e522af0ef45b544ffee4cef
anumberplate <aluma.kyle@gmail.com>
2025-07-30 02:09:43 +0300

    MVC REST API

--- server.js
+++ server.js
@@ -2,26 +2,15 @@ const express = require('express')
 const path = require('path')
 const cors = require('cors')
 const app = express()
+const corsOptions = require('./config/corsOptions')
 const errorHandler = require('./middleware/errorHandler')
 const { logger } = require('./middleware/logEvents')
 const PORT = process.env.PORT || 3500
 
-//custom middleware logger
+
 
 app.use(logger)
 
-const whitelist = ['https://www.yoursite.com','http://127.0.0.1:5500', 'http://localhost:3500']
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin){
-      callback(null, true)
-    } else{
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions))
 
 app.use(express.urlencoded({extended: false}))
@@ -29,11 +18,8 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/subdir',express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
-app.use('/subdir', require('./routes/subdir'))
-app.use('/employees', require('./routes/api/employees'))
 
 
 
@@ -49,11 +35,11 @@ app.all(/\/*/, (req, res) => {
   
 })
 
+app.use(errorHandler)
+
+// add listener for the log event
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
-
-app.use(errorHandler)
-// add listener for the log event
 
 
 
